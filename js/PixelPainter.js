1b8c206cf327295212d654c3f0d3dfe9bc8f5a91
Jason <Jason@Colins-MacBook-Pro.local>
2018-07-08 22:34:02 -1000

    completed hacky erase button functionality

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -10,33 +10,67 @@ let buttonBox = document.createElement('div'); //creates button box
 buttonBox.id = 'buttonBoxId';
 paletteBox.appendChild(buttonBox)
 
-let eraseButton = document.createElement('button'); //erase button
+let eraseButton = document.createElement('button'); //creates erase button
 eraseButton.id = 'eraseButtonId';
 eraseButton.innerHTML = 'Erase';
+eraseButton.addEventListener('click', eraseColor) //erase button eventListener
 buttonBox.appendChild(eraseButton);
-let clearButton = document.createElement('button'); //clear button
+
+function eraseColor() {
+    if(whiteColorVar === false) {
+        whiteColorVar = true;
+    } else {
+        whiteColorVar = false;
+    }
+}
+
+
+// function eraseColor() { //needs access to gridSquaresVar object?
+//     for(let i = 0; i < gridSquaresVar.length; i++) {
+//         let gridSquares = document.createElement('div'); //why can't I add changeColorMouseover to this for loop?
+//         gridSquares.className = 'gridSquaresClass';
+//         gridSquares.addEventListener('click', eraseColorClick)
+//         gridSquaresVar[i].style.backgroundColor = 'white';
+//         gridBox.appendChild(gridSquares)
+//     }
+// }
+
+// function eraseColorClick() {
+//     whiteColorVar.style.backgroundColor = 'white';
+//     console.log('fired')
+// }
+
+let clearButton = document.createElement('button'); //creates clear button
 clearButton.id = 'clearButtonId';
 clearButton.innerHTML = 'Clear';
+clearButton.addEventListener('click', clearColor) //clear color eventListener
 buttonBox.appendChild(clearButton);
+
+function clearColor() { //clear color for loop, would like to use only callback
+    for(let i = 0; gridSquaresVar.length; i++) {
+        gridSquaresVar[i].style.backgroundColor = 'white';
+    }
+}
+
 
 let paletteArray = ['Aliceblue', 'AntiqueWhite', 'b30000', 'Aqua', 'ff1a66', '66ff66', '99004d' , 'Aquamarine', 'Azure', 'orange' , 'black' , 'red' , 'yellow' , 'blue' , 'indigo' , 'violet' , 'blueviolet' , 'brown' , 'blueviolet' , 'cadetblue' , 'chocolate' , 'coral' , 'cornflowerblue' , 'crimson' , 'cyan' , 'darkblue' , 'darkcyan' , 'darkmagenta' , 'darkgoldenrod' , 'darkorchid' , 'darkslateblue' , 'darkseagreen' , 'darkslateblue' , 'darkslategrey' , 'DeepSkyBlue' , 'firebrick' , 'fucia' , 'DimGray' , 'gold' , 'DimGray','ivory' , 'Khaki' , 'lavender' , 'LightCoral' , 'limegreen' , 'mediumblue' , 'MediumSlateBlue' , 'orangered' , 'saddlebrown' , 'springgreen']
 let paletteSquareVar = document.getElementsByClassName('paletteSquareClass'); //palette grid divs
-let setColorVar; 
+let setColorVar; //color saving variable
+let whiteColorVar = false; //a variable that allows erasing click ability
+let gridSquaresVar = document.getElementsByClassName('gridSquaresClass'); //pixel grid divs
 
-for(let i = 0; i < 50; i++) { //palette modifying for loop
+for(let i = 0; i < 50; i++) { //palette modifying for loop //why couldn't I addEventListener to this for loop?
     let paletteSquares = document.createElement('div');
     paletteSquares.className = 'paletteSquareClass';
     paletteBox.appendChild(paletteSquares)
 }
 
-for(let i = 0; i < paletteSquareVar; i++) {
+for(let i = 0; i < paletteSquareVar.length; i++) { //color set addEventListener for loop
     paletteSquareVar[i].addEventListener('click', setColor);
     
 }
-
-function setColor() { //change color callback
+function setColor() { //change color callback, should it go in for loop?
     setColorVar = this.style.backgroundColor;
-    console.log('fired');
 }
 
 for (let i = 0; i < paletteArray.length; i++) { //palette color changing for loop
@@ -44,7 +78,30 @@ for (let i = 0; i < paletteArray.length; i++) { //palette color changing for loo
 }
 
 for(let i = 0; i < 1080; i++) { //pixel grid modifying for loop
-    let gridSquares = document.createElement('div');
+    let gridSquares = document.createElement('div'); //why can't I add changeColorMouseover to this for loop?
     gridSquares.className = 'gridSquaresClass';
     gridBox.appendChild(gridSquares);
+}
+
+for(let i = 0; i < 1080; i++) { //pixel grid color changing eventListener
+    gridSquaresVar[i].addEventListener('mouseover', changeColorMouseover)
+    gridSquaresVar[i].addEventListener('click', whiteBackgroundClick)
+    gridSquaresVar[i].addEventListener('mousedown', whiteBackgroundMouseDown)
+}
+
+function changeColorMouseover() { //color changing callback
+    this.style.backgroundColor = setColorVar;
+}
+
+function whiteBackgroundClick() {
+    if(whiteColorVar === true) {
+        this.style.backgroundColor = 'white';
+    }
+    
+}
+
+function whiteBackgroundMouseDown() {
+    if(whiteColorVar === true) {
+        this.style.backgroundColor = 'white';
+    }
 }
