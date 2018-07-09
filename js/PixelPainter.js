07d93a7ecb36b2219086a413cea6a0de129577b1
Jason <Jason@Colins-MacBook-Pro.local>
2018-07-08 19:15:21 -1000

    trying to add eventlistener to paletteSquareVar

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,40 +1,49 @@
-let paletteBox = document.createElement('div');
+let paletteBox = document.createElement('div'); //creates palette box
 paletteBox.id = 'paletteId';
 pixelPainter.appendChild(paletteBox);
 
-let gridBox = document.createElement('div');
+let gridBox = document.createElement('div'); //creates pixel grid box
 gridBox.id = 'gridBoxId';
 pixelPainter.appendChild(gridBox);
 
-let buttonBox = document.createElement('div');
+let buttonBox = document.createElement('div'); //creates button box
 buttonBox.id = 'buttonBoxId';
 paletteBox.appendChild(buttonBox)
 
-let eraseButton = document.createElement('button');
+let eraseButton = document.createElement('button'); //erase button
 eraseButton.id = 'eraseButtonId';
 eraseButton.innerHTML = 'Erase';
 buttonBox.appendChild(eraseButton);
-let clearButton = document.createElement('button');
+let clearButton = document.createElement('button'); //clear button
 clearButton.id = 'clearButtonId';
 clearButton.innerHTML = 'Clear';
 buttonBox.appendChild(clearButton);
 
-for(let i = 0; i < 50; i++) {
+let paletteArray = ['Aliceblue', 'AntiqueWhite', 'b30000', 'Aqua', 'ff1a66', '66ff66', '99004d' , 'Aquamarine', 'Azure', 'orange' , 'black' , 'red' , 'yellow' , 'blue' , 'indigo' , 'violet' , 'blueviolet' , 'brown' , 'blueviolet' , 'cadetblue' , 'chocolate' , 'coral' , 'cornflowerblue' , 'crimson' , 'cyan' , 'darkblue' , 'darkcyan' , 'darkmagenta' , 'darkgoldenrod' , 'darkorchid' , 'darkslateblue' , 'darkseagreen' , 'darkslateblue' , 'darkslategrey' , 'DeepSkyBlue' , 'firebrick' , 'fucia' , 'DimGray' , 'gold' , 'DimGray','ivory' , 'Khaki' , 'lavender' , 'LightCoral' , 'limegreen' , 'mediumblue' , 'MediumSlateBlue' , 'orangered' , 'saddlebrown' , 'springgreen']
+let paletteSquareVar = document.getElementsByClassName('paletteSquareClass'); //palette grid divs
+let setColorVar; 
+
+for(let i = 0; i < 50; i++) { //palette modifying for loop
     let paletteSquares = document.createElement('div');
     paletteSquares.className = 'paletteSquareClass';
-    // paletteSquareVar.addEventListener('click', changeColor);
     paletteBox.appendChild(paletteSquares)
 }
-// function changeColor() {
 
-// }
-let paletteArray = ['Aliceblue', 'AntiqueWhite', 'b30000', 'Aqua', 'ff1a66', '66ff66', '99004d' , 'Aquamarine', 'Azure', 'orange' , 'black' , 'red' , 'yellow' , 'blue' , 'indigo' , 'violet' , 'blueviolet' , 'brown' , 'blueviolet' , 'cadetblue' , 'chocolate' , 'coral' , 'cornflowerblue' , 'crimson' , 'cyan' , 'darkblue' , 'darkcyan' , 'darkmagenta' , 'darkgoldenrod' , 'darkorchid' , 'darkslateblue' , 'darkseagreen' , 'darkslateblue' , 'darkslategrey' , 'DeepSkyBlue' , 'firebrick' , 'fucia' , 'DimGray' , 'gold' , 'DimGray','ivory' , 'Khaki' , 'lavender' , 'LightCoral' , 'limegreen' , 'mediumblue' , 'MediumSlateBlue' , 'orangered' , 'saddlebrown' , 'springgreen']
-let paletteSquareVar = document.getElementsByClassName('paletteSquareClass');
-for (let i = 0; i < paletteArray.length; i++) {
+for(let i = 0; i < paletteSquareVar; i++) {
+    paletteSquareVar[i].addEventListener('click', setColor);
+    
+}
+
+function setColor() { //change color callback
+    setColorVar = this.style.backgroundColor;
+    console.log('fired');
+}
+
+for (let i = 0; i < paletteArray.length; i++) { //palette color changing for loop
     paletteSquareVar[i].style.backgroundColor = paletteArray[i];
 }
 
-for(let i = 0; i < 1080; i++) {
+for(let i = 0; i < 1080; i++) { //pixel grid modifying for loop
     let gridSquares = document.createElement('div');
     gridSquares.className = 'gridSquaresClass';
     gridBox.appendChild(gridSquares);
